2cf2bf4874ef1a9853bcfcd0a25bd2ef61b6dcb9
mamun8893 <0316361936@student.wub.edu.bd>
2022-08-05 23:44:12 +0600

    setup:project setup & gobal css defined

---
+++ components/header/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+  return (
+    <div className="header-area">
+      <div className="container">
+        <div className="header-wraper">
+          <div className="header-left">
+            <div className="navbar-brand"></div>
+            <div className="navbar">
+              <ul className="navbar-nav">
+                <li className="nav-item"></li>
+              </ul>
+            </div>
+          </div>
+          <div className="header-right"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
